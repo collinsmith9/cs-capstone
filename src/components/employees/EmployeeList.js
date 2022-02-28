46513c877c7b69f090442a3595d49de980257aaa
collin <geniuscollin@gmail.com>
2022-02-28 00:27:41 -0600

    employee view working, profile working, hr getting close

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react"
-import { getEmployees } from "../apiManager"
+import { getUsers } from "../apiManager"
+// import { getEmployees } from "../apiManager"
 import './EmployeeList.css'
 
 
 
 export const EmployeeList = () => {
-    const [employees, setEmployees] = useState([])
+    const [users, setUsers] = useState([])
+    // const [employees, setEmployees] = useState([])
 
     useEffect(() => {
-        getEmployees()
-        .then(setEmployees)
+        getUsers()
+        .then(setUsers)
 
     },
     [])
+
+    const employees = users.filter((user) => {
+        if (user.user.is_staff === true) {
+            return true
+        }
+        return false
+    })
 
     return (
         <>
@@ -20,8 +29,8 @@ export const EmployeeList = () => {
         {
             employees.map((emp) => {
                 return <fieldset key={`employee--${emp.id}`}  className="employee">
-                <div><h4>{emp.name}</h4>
-                <h5>Email: {emp.email}</h5>
+                <div><h4>{emp.user.first_name}</h4>
+                <h5>Email: {emp.user.email}</h5>
                 </div>
                 </fieldset>
 
